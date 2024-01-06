67e877ac9900f3f2ebf44acae2ede944f9abf5e7
hecticme <hangoc.minh1306@gmail.com>
2024-01-06 20:36:45 +0700

    Add playback icons and their styles

--- src/components/ThePlaybackControls.vue
+++ src/components/ThePlaybackControls.vue
@@ -1,14 +1,20 @@
 <script setup>
-
+import { Icon } from '@iconify/vue';
 </script>
 
 <template>
 <div class="playback-controls">
   <button class="playback-button button-play">
-
+    <Icon
+      icon="material-symbols:play-arrow-rounded"
+      class="playback-icon"
+    />
   </button>
   <button class="playback-button button-reset">
-
+    <Icon
+      icon="material-symbols:restart-alt-rounded"
+      class="playback-icon"
+    />
   </button>
 </div>
 </template>
@@ -21,10 +27,27 @@
 }
 
 .playback-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 2.5rem;
   aspect-ratio: 1;
   border-radius: 50%;
   color: var(--color-white);
   background-color: var(--color-black);
+  transition-property: transform, color, background-color;
+  transition-duration: 150ms;
+  transition-timing-function: var(--cubic-bezier);
+}
+
+.playback-button:is(:hover, :focus) {
+  transform: translateY(-3px);
+  color: var(--color-black);
+  background-color: var(--color-white);
+  outline: 1px solid var(--color-black);
+}
+
+.playback-icon {
+  font-size: 1.3rem;
 }
 </style>

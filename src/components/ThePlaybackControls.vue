0a317ee054292b3943e2536655b4766e4b358958
hecticme <hangoc.minh1306@gmail.com>
2024-01-07 12:09:02 +0700

    Update resume/pause button logic and styles

--- src/components/ThePlaybackControls.vue
+++ src/components/ThePlaybackControls.vue
@@ -1,15 +1,40 @@
 <script setup>
 import { Icon } from '@iconify/vue';
+import { ref } from 'vue';
+
+const isPlaying = ref(false);
+
+function handleResumePause() {
+  isPlaying.value = !isPlaying.value;
+}
 </script>
 
 <template>
 <div class="playback-controls">
-  <button class="playback-button button-play">
-    <Icon
-      icon="material-symbols:play-arrow-rounded"
-      class="playback-icon"
-    />
+  <button
+    class="playback-button button-play"
+    @click="handleResumePause"
+  >
+    <div class="playback-icon-anchor">
+      <div
+        class="playback-icon-wrapper"
+        :class="{
+          'is-playing': isPlaying,
+        }"
+      >
+        <Icon
+          icon="material-symbols:play-arrow-rounded"
+          class="playback-icon"
+        />
+
+        <Icon
+          icon="material-symbols:pause-rounded"
+          class="playback-icon"
+        />
+      </div>
+    </div>
   </button>
+
   <button class="playback-button button-reset">
     <Icon
       icon="material-symbols:restart-alt-rounded"
@@ -38,6 +63,7 @@ import { Icon } from '@iconify/vue';
   transition-property: transform, color, background-color;
   transition-duration: 150ms;
   transition-timing-function: var(--cubic-bezier);
+  --playback-icon-size: 1.3rem;
 }
 
 .playback-button:is(:hover, :focus) {
@@ -47,7 +73,30 @@ import { Icon } from '@iconify/vue';
   outline: 1px solid var(--color-black);
 }
 
+.playback-icon-anchor {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: var(--playback-icon-size);
+  aspect-ratio: 1;
+  overflow: hidden;
+}
+
+.playback-icon-wrapper {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  top: 0;
+  transition-property: top;
+  transition-duration: 150ms;
+  transition-timing-function: var(--cubic-bezier);
+}
+
+.playback-icon-wrapper.is-playing {
+  top: -100%;
+}
+
 .playback-icon {
-  font-size: 1.3rem;
+  font-size: var(--playback-icon-size);
 }
 </style>

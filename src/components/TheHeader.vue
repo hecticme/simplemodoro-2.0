172ab8e9dfe53a63b6784023602f69a2c0d2e557
hecticme <hangoc.minh1306@gmail.com>
2024-01-14 10:49:50 +0700

    Add Github icon for repo link

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import IconGithubMark from '../assets/icons/IconGithubMark.vue';
 </script>
 
 <template>
@@ -9,7 +9,9 @@
   </div>
 
   <div>
-    <p>Github</p>
+    <a href="https://github.com/">
+      <IconGithubMark class="icon-github" />
+    </a>
   </div>
 </header>
 </template>
@@ -21,5 +23,18 @@
   gap: 1rem;
   margin-block: 1.5rem 2.5rem;
   width: 100%;
+}
+
+.icon-github {
+  width: 1rem;
+  color: #151515;
+  cursor: pointer;
+  transition-property: transform;
+  transition-duration: 150ms;
+  transition-timing-function: var(--transition-cubic-bezier);
+}
+
+.icon-github:hover {
+  transform: scale(1.1);
 }
 </style>

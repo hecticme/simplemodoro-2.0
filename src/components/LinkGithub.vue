1acb6aacc6ef94cffa0567b27abc5dc87c87c283
hecticme <hangoc.minh1306@gmail.com>
2024-02-12 10:05:43 +0700

    style: update `LinkGithub` style
    This changed the Github link's appearance to look like a rounded button.

--- src/components/LinkGithub.vue
+++ src/components/LinkGithub.vue
@@ -7,12 +7,26 @@ import IconGithubMark from '../assets/icons/IconGithubMark.vue'
   href="https://github.com/"
   target="_blank"
   rel="noreferrer"
+  class="icon-github-container"
 >
   <IconGithubMark class="icon-github" />
 </a>
 </template>
 
 <style scoped>
+.icon-github-container {
+  padding: 0.5rem;
+  border-radius: 50%;
+  transition-property: background-color;
+  transition-duration: 150ms;
+  transition-timing-function: ease-in-out;
+}
+
+.icon-github-container:is(:hover, :focus-visible) {
+  background-color: #eeeeee;
+  outline: none;
+}
+
 .icon-github {
   width: 1rem;
   color: #151515;
@@ -20,9 +34,5 @@ import IconGithubMark from '../assets/icons/IconGithubMark.vue'
   transition-property: transform;
   transition-duration: 150ms;
   transition-timing-function: var(--transition-cubic-bezier);
-}
-
-.icon-github:hover {
-  transform: scale(1.1);
 }
 </style>

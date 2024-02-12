7bd94242df08c7bd8a6e02d646390da4f5293761
hecticme <hangoc.minh1306@gmail.com>
2024-02-12 09:53:55 +0700

    feat(ui): add theme button

---
+++ src/components/ButtonTheme.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+  <div class="theme-container">
+    <button class="button-theme">
+      <Icon
+        icon="material-symbols:sunny-outline-rounded"
+        class="icon-theme"
+      />
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.theme-container {
+  position: relative;
+}
+
+.button-theme {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  aspect-ratio: 1 / 1;
+  border-radius: 50%;
+  background-color: #fff;
+  transition-property: background-color;
+  transition-duration: 150ms;
+  transition-timing-function: ease-in-out;
+}
+
+.button-theme:is(:hover, :focus-visible) {
+  background-color: #eeeeee;
+  outline: none;
+}
+
+.icon-theme {
+  font-size: 1rem;
+}
+</style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,18 +1,23 @@
 <script setup>
 import IconGithubMark from '../assets/icons/IconGithubMark.vue'
+import ButtonTheme from './ButtonTheme.vue'
 </script>
 
 <template>
 <header class="header">
   <p class="logo-text">Simplemodoro.</p>
 
-  <a
-    href="https://github.com/"
-    target="_blank"
-    rel="noreferrer"
-  >
-    <IconGithubMark class="icon-github" />
-  </a>
+  <div class="header-utils">
+    <ButtonTheme />
+
+    <a
+      href="https://github.com/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <IconGithubMark class="icon-github" />
+    </a>
+  </div>
 </header>
 </template>
 
@@ -23,6 +28,11 @@ import IconGithubMark from '../assets/icons/IconGithubMark.vue'
   gap: 1rem;
   margin-block: 1.5rem 2.5rem;
   width: 100%;
+}
+
+.header-utils {
+  display: flex;
+  gap: 0.75rem;
 }
 
 .logo-text {

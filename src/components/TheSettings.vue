2fb4b2520dc039b40d320bb393515954567c454c
hecticme <hangoc.minh1306@gmail.com>
2024-02-23 00:59:30 +0700

    feat: create TheSettings.vue

---
+++ src/components/TheSettings.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+// Import components
+import SettingsForm from './SettingsForm.vue'
+import SettingsButton from './SettingsButton.vue'
+
+const isOpen = ref(false)
+
+function toggleSettingsForm () {
+  isOpen.value = !isOpen.value
+}
+</script>
+
+<template>
+<div class="settings">
+  <SettingsButton
+    :is-open="isOpen"
+    @toggle-settings-form="toggleSettingsForm"
+  />
+
+  <SettingsForm
+    :is-open="isOpen"
+  />
+</div>
+</template>
+
+<style scoped>
+.settings {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  border-radius: 4px;
+}
+</style>

eca32890d34fdecf1574e7564989fb4d87575308
hecticme <hangoc.minh1306@gmail.com>
2024-02-24 11:30:21 +0700

    style: implement animation for the settings modal
    This commits added a fly-in animation when opening the settings modal.

--- src/components/SettingsForm.vue
+++ src/components/SettingsForm.vue
@@ -22,57 +22,60 @@ const breakDuration = ref(pomodoro.breakDuration / 60)
 </script>
 
 <template>
-<section
-  v-if="isOpen"
-  class="settings-form-wrapper"
+<Transition
+  name="fly-in"
+  :duration="250"
 >
-  <!-- This `div` is for turning off the modal when clicking outside -->
-  <div
-    @click="$emit('toggleSettingsForm')"
-    class="settings-background"
-  ></div>
-
-  <div class="settings-form">
-    <div class="input-container">
-      <label
-        for="focus-duration"
-        class="label"
-      >
-        Focus session duration
-      </label>
-      <input
-        v-model="focusDuration"
-        type="number"
-        id="focus-duration"
-        name="focus-duration"
-        class="input-session"
-      />
-    </div>
-
-    <div class="input-container">
-      <label
-        for="break-duration"
-        class="label"
-      >
-        Break session duration
-      </label>
-      <input
-        v-model="breakDuration"
-        type="number"
-        id="break-duration"
-        name="break-duration"
-        class="input-session"
-      />
-    </div>
-
-    <button
+  <section
+    v-if="isOpen"
+    class="settings-form-wrapper"
+  >
+    <!-- This `div` is for turning off the modal when clicking outside -->
+    <div
       @click="$emit('toggleSettingsForm')"
-      class="settings-save"
-    >
-      Save
-    </button>
-  </div>
-</section>
+      class="settings-background"
+    ></div>
+
+    <div class="settings-form">
+      <div class="input-container">
+        <label
+          for="focus-duration"
+          class="label"
+        >
+          Focus session duration
+        </label>
+        <input
+          v-model="focusDuration"
+          type="number"
+          id="focus-duration"
+          name="focus-duration"
+          class="input-session"
+        />
+      </div>
+      <div class="input-container">
+        <label
+          for="break-duration"
+          class="label"
+        >
+          Break session duration
+        </label>
+        <input
+          v-model="breakDuration"
+          type="number"
+          id="break-duration"
+          name="break-duration"
+          class="input-session"
+        />
+      </div>
+      <button
+        @click="$emit('toggleSettingsForm')"
+        class="settings-save"
+      >
+        Save
+      </button>
+    </div>
+  </section>
+</Transition>
 </template>
 
 <style scope>
@@ -99,6 +102,19 @@ const breakDuration = ref(pomodoro.breakDuration / 60)
   padding: 1rem 1.25rem;
   border-radius: 4px;
   z-index: 1;
+}
+
+.fly-in-enter-active .settings-form,
+.fly-in-leave-active .settings-form {
+  transition-property: transform, opacity;
+  transition-duration: 250ms;
+  transition-timing-function: var(--transition-cubic-bezier);
+}
+
+.fly-in-enter-from .settings-form,
+.fly-in-leave-to .settings-form {
+  transform: translateY(20px);
+  opacity: 0;
 }
 
 .input-container {

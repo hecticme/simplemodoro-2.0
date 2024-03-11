099f2800fc9d2af4bc1556099f9e75bee6867ea4
hecticme <hangoc.minh1306@gmail.com>
2024-03-11 23:52:37 +0700

    style: change button UI
    Changed the color of buttons. Removed outline on hover.

--- src/components/ButtonBase.vue
+++ src/components/ButtonBase.vue
@@ -6,27 +6,25 @@
 
 <style scoped>
 .button-base {
-  color: var(--color-white-100);
-  background-color: var(--color-black-900);
-  transition-property: color, background-color, outline;
+  color: var(--color-black-900);
+  background-color: var(--color-white-100);
+  box-shadow: var(--box-shadow-button-dark);
+  transition-property: color, background-color;
   transition-duration: 250ms;
   transition-timing-function: var(--transition-cubic-bezier);
 }
 
 .button-base:is(:hover, :focus-visible) {
-  color: var(--color-black-900);
-  background-color: var(--color-white-100);
-  outline: 1px solid var(--color-black-900);
+  background-color: var(--color-gray-100);
 }
 
 .dark .button-base {
-  color: var(--color-black-800);
-  background-color: var(--color-white-200);
+  color: var(--color-white-200);
+  background-color: var(--color-black-800);
+  box-shadow: var(--box-shadow-button-light);
 }
 
 .dark .button-base:is(:hover, :focus-visible) {
-  color: var(--color-white-200);
-  background-color: var(--color-black-800);
-  outline: 1px solid var(--color-white-200);
+  background-color: var(--color-black-700);
 }
 </style>

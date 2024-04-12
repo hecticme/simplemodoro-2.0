5559bf05c0043e91cbc0758bbee0448c583d2883
hecticme <hangoc.minh1306@gmail.com>
2024-04-12 18:00:45 +0700

    style(break-session): add decorative underline for some texts

--- src/components/the/TheHeader.vue
+++ src/components/the/TheHeader.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 import ButtonTheme from '@/src/components/button/ButtonTheme.vue'
 import LinkGithub from '@/src/components/LinkGithub.vue'
+import { usePomodoroStore } from '@/src/stores/pomodoro'
+
+const pomodoro = usePomodoroStore()
 </script>
 
 <template>
   <header class="header">
-    <p class="logo-text">
+    <p
+      class="logo-text"
+      :class="{
+        'isBreak': pomodoro.isBreak,
+      }"
+    >
       Simplemodoro.
     </p>
 
@@ -33,7 +41,23 @@ import LinkGithub from '@/src/components/LinkGithub.vue'
 }
 
 .logo-text {
+  position: relative;
   font-size: 0.75rem;
   font-weight: 700;
+}
+
+.logo-text::after {
+  content: '';
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 0;
+  height: 2px;
+  background-color: var(--color-emerald-500);
+  transition: width 250ms var(--transition-cubic-bezier);
+}
+
+.logo-text.isBreak::after {
+  width: 70%;
 }
 </style>

97405d3303cebecd8649ecb079c757cac9fcf05a
hecticme <hangoc.minh1306@gmail.com>
2024-04-12 12:31:32 +0700

    refacor(ts): use TS for formatTime functions

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,18 +1,17 @@
 /**
- * @param {number} time - A unit of time.
- * @returns {string} Add `0` at the start for single digits.
+ * Add '0' to the start of single digits.
  */
-function normalizedTimeUnit (time) {
+function normalizedTimeUnit (time: number) {
   return time.toString().padStart(2, '0')
 }
 
 /**
- * @param {number} time - A duration in seconds.
- * @returns {string} A formatted time string.
+ * Format time in number format to readable clock-like string.
+ *
+ * @example formatTime(25 * 60) // '25:00'
  */
-export default function formatTime (time) {
+export default function formatTime (time: number) {
   const minute = Math.floor(time / 60)
   const second = time % 60
-
   return `${normalizedTimeUnit(minute)}:${normalizedTimeUnit(second)}`
 }
